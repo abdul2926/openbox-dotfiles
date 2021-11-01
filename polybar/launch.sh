217544e0846f126500229abe009a0356608db8be
abdul2926 <90007948+abdul2926@users.noreply.github.com>
2021-11-01 20:16:28 +0100

    Add polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch Polybar. Config location ~/.config/polybar/config.ini
+polybar spark --config=~/.config/polybar/config.ini 2>&1 | tee -a /temp/polybar.log & disown
+
+# Automatically add polybar to all monitors
+# For more info: https://github.com/polybar/polybar/issues/763
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " f1); do
+		MONITOR=$m polybar --reload spark &
+	done
+else
+	polybar --reload spark &
+fi
+
